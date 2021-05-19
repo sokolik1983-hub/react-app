2778e957ec6a6d6cee98c5dbfbcb0a9f42728bb2
Aleksey Sokolov <lionsbet@outlook.com>
2021-05-19 16:23:52 +0300

    adding files

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="http://pngimg.com/uploads/lamborghini/lamborghini_PNG10709.png" alt="logo"/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://global.unitednations.entermediadb.net/assets/mediadb/services/module/asset/downloads/preset/Libraries/Production+Library/19-03-2020_Unsplash_Forest.jpg/image1170x530cropped.jpg" alt="img"/>
+            </div>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                my-posts
+                <div>
+                    new-post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
